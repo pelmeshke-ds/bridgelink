466a85437bdbdbd17512da41e1981995550b961d
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-08-14 19:35:34 +0300

    feat basic data structures

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,3 +1,34 @@
+#define BUTTON_AMOUNT 16
+
+enum button_state {
+    INACTIVE,
+    PRESSED,
+    HELD,
+    RELEASED,
+};
+
+struct StickData {
+    int16_t x_axis;
+    int16_t y_axis;
+    button_state button;
+}
+
+struct GyroscopeData {
+    int16_t accelerometer_x;
+    int16_t accelerometer_y;
+    int16_t accelerometer_z;
+    int16_t gyroscope_x;
+    int16_t gyroscope_y;
+    int16_t gyroscope_z;
+}
+
+struct InputData {
+    button_state buttons[BUTTON_AMOUNT];
+    StickData left_stick;
+    StickData right_stick;
+    GyroscopeData gyroscope_data;
+};
+
 void setup() {
     Serial1.begin(9600);
     Serial1.println("Start");
